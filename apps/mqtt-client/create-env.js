a6b4e610b2f7f696440859f50c1ac1e07fa280d3
Schild, Philippe <Philippe.Schild@adesso.de>
2020-10-27 20:02:16 +0100

    added auth for mqtt-client

--- apps/mqtt-client/create-env.js
+++ apps/mqtt-client/create-env.js
@@ -11,7 +11,9 @@ try {
 }
 
 const PUBLIC_ENDPOINT = envContent.match(/PUBLIC_ENDPOINT=(.+)/)[1];
-const envTargetContent = { PUBLIC_ENDPOINT };
+const SERVICE_USER = envContent.match(/SERVICE_USER=(.+)/)[1];
+const SERVICE_PASSWORD = envContent.match(/SERVICE_PASSWORD=(.+)/)[1];
+const envTargetContent = { PUBLIC_ENDPOINT, SERVICE_USER, SERVICE_PASSWORD };
 
 const envTargetPath = resolve(__dirname, 'src', 'environments', 'env.json');
 try {
--- apps/mqtt-client/src/app/http.service.ts
+++ apps/mqtt-client/src/app/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 
@@ -10,8 +10,16 @@ export class HttpService {
   constructor(private httpClient: HttpClient) {}
 
   get(speachText: string): void {
+    // TODO: interceptor
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa(`${environment.env.SERVICE_USER}:${environment.env.SERVICE_PASSWORD}`)
+      })
+    };
+
     const formattedSpeachText = encodeURI(speachText);
-    this.httpClient.get(`${environment.env.PUBLIC_ENDPOINT}:3333/alexa/speak/${formattedSpeachText}`).subscribe(console.log);
+    this.httpClient.get(`${environment.env.PUBLIC_ENDPOINT}:3333/alexa/speak/${formattedSpeachText}`, httpOptions).subscribe(console.log);
   }
 
 }
