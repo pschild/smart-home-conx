6b1f67363edcbba46633e99f0fcf93d3679e193e
Schild, Philippe <Philippe.Schild@adesso.de>
2020-11-23 22:19:06 +0100

    fixed url

--- apps/esp-pir-manager/src/app/solar-times.ts
+++ apps/esp-pir-manager/src/app/solar-times.ts
@@ -13,17 +13,17 @@ export interface SolarTimes {
 let CACHE: SolarTimes = null;
 
 export const getSolarTimesForDate$ = (date: Date): Observable<SolarTimes> => {
-  const dateKey = format(date, 'yyyy-MM-dd');
+  const ymd = format(date, 'yyyy-MM-dd');
   log(`getSolarTimesFor: ${date}`);
-  const apiCall$ = from(axios.get(`https://api.sunrise-sunset.org/json?lat=51.668189&lng=6.148282&date=${date}&formatted=0`)).pipe(
+  const apiCall$ = from(axios.get(`https://api.sunrise-sunset.org/json?lat=51.668189&lng=6.148282&date=${ymd}&formatted=0`)).pipe(
     map(result => {
       const { sunrise, sunset } = result.data.results;
-      CACHE = { key: dateKey, sunrise: new Date(sunrise), sunset: new Date(sunset) };
+      CACHE = { key: ymd, sunrise: new Date(sunrise), sunset: new Date(sunset) };
       log(`made API request and set cache: ${JSON.stringify(CACHE)}`);
       return CACHE;
     })
   );
   const cache$ = of(CACHE).pipe(tap(() => log(`used cache: ${JSON.stringify(CACHE)}`)));
 
-  return iif(() => !!CACHE && CACHE.key === dateKey, cache$, apiCall$);
+  return iif(() => !!CACHE && CACHE.key === ymd, cache$, apiCall$);
 }
