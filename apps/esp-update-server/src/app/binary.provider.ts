a27a2441e0695603afdf8591c7ae14fd2987fca3
Schild, Philippe <Philippe.Schild@adesso.de>
2020-11-23 17:39:47 +0100

    fixed origin check

--- apps/esp-update-server/src/app/binary.provider.ts
+++ apps/esp-update-server/src/app/binary.provider.ts
@@ -22,15 +22,15 @@ export const findBinaryForUpdate = async (chipId: string): Promise<string | unde
 };
 
 export const originateFromEsp = headers => {
-  return !headers['user-agent']
-  || headers['user-agent'] !== 'ESP8266-http-Update'
-  || !headers['x-esp8266-chip-id'] // ex. 226047
-  || !headers['x-esp8266-sta-mac'] // ex. 4C:11:AE:03:72:FF
-  || !headers['x-esp8266-free-space'] // ex. 462848
-  || !headers['x-esp8266-sketch-size'] // ex. 301408
-  || !headers['x-esp8266-sketch-md5'] // ex. 18f8dc4ccb5bbb8b909a7ed1a8b5f173
-  || !headers['x-esp8266-chip-size'] // ex. 1048576
-  || !headers['x-esp8266-sdk-version'] // ex. 2.2.2-dev(38a443e)
-  // || !headers['x-esp8266-version'] // ex. esp-mqtt_v0.2.3
+  return headers['user-agent']
+  && headers['user-agent'] === 'ESP8266-http-Update'
+  && headers['x-esp8266-chip-id'] // ex. 226047
+  && headers['x-esp8266-sta-mac'] // ex. 4C:11:AE:03:72:FF
+  && headers['x-esp8266-free-space'] // ex. 462848
+  && headers['x-esp8266-sketch-size'] // ex. 301408
+  && headers['x-esp8266-sketch-md5'] // ex. 18f8dc4ccb5bbb8b909a7ed1a8b5f173
+  && headers['x-esp8266-chip-size'] // ex. 1048576
+  && headers['x-esp8266-sdk-version'] // ex. 2.2.2-dev(38a443e)
+  // && headers['x-esp8266-version'] // ex. esp-mqtt_v0.2.3
   ;
 };
