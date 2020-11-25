d378f5d889b6267bcc91c39f58abcb19df995e5d
Schild, Philippe <Philippe.Schild@adesso.de>
2020-11-25 17:05:17 +0100

    improved bash script

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ if [[ !($(which docker) && $(docker --version)) ]];
 
     echo "Change password..."
     passwd
+    if [[ !($? -eq 0) ]];
+      then
+        echo "ERROR: Changing password did not succeed."
+        exit 1
 
     echo "Updating system..."
     sudo apt-get update
@@ -20,39 +24,68 @@ if [[ !($(which docker) && $(docker --version)) ]];
   else
     echo "docker installation detected. Script will now continue..."
 
-    echo "Installing docker-compose..."
-    sudo apt install python3-pip -y
-    sudo pip3 install docker-compose
+    if [[ !($(which docker-compose) && $(docker-compose --version)) ]];
+      then
+        echo "Installing docker-compose..."
+        sudo apt install python3-pip -y
+        sudo pip3 install docker-compose
+      else
+        echo "docker-compose is already installed. Skipping."
+    fi
 
-    echo "Installing git..."
-    sudo apt install git -y
-    git config --global user.email "philippe.schild@googlemail.com"
-    git config --global user.name "pschild"
+    if [[ !($(which docker-compose) && $(docker-compose --version)) ]];
+      then
+        echo "Installing git..."
+        sudo apt install git -y
+        git config --global user.email "philippe.schild@googlemail.com"
+        git config --global user.name "pschild"
+      else
+        echo "git is already installed. Skipping."
+    fi
 
     echo "Checking out smart-home-conx..."
     cd ~
     git clone https://github.com/pschild/smart-home-conx
     cd smart-home-conx
-    echo "Preparing env file..."
-    echo "Please provide values for your secret env properties:"
-    read -p "SERVICE_USER (e.g. me): " serviceUserPrompt
-    read -p "SERVICE_PASSWORD (e.g. s3cr3t): " servicePasswordPrompt
-    read -p "AMAZON_EMAIL (e.g. amazon_account@email.address): " amazonEmailPrompt
-    read -p "AMAZON_PASSWORD (e.g. Very_Secret_Amazon_Account_Password): " amazonPasswordPrompt
-    read -p "AMAZON_MFA_SECRET (e.g. 1234 5678 9ABC DEFG HIJK LMNO PQRS TUVW XYZ0 1234 5678 9ABC DEFG): " amazonMfaPrompt
-    touch .env
-    echo "SERVICE_USER=${serviceUserPrompt}" >> .env
-    echo "SERVICE_PASSWORD=${servicePasswordPrompt}" >> .env
-    echo "AMAZON_EMAIL=${amazonEmailPrompt}" >> .env
-    echo "AMAZON_PASSWORD=${amazonPasswordPrompt}" >> .env
-    echo "AMAZON_MFA_SECRET=${amazonMfaPrompt}" >> .env
+    if [[ ! -f .env ]];
+      then
+        echo "Preparing env file..."
+        echo "Please provide values for your secret env properties:"
+        read -p "SERVICE_USER (e.g. me): " serviceUserPrompt
+        read -p "SERVICE_PASSWORD (e.g. s3cr3t): " servicePasswordPrompt
+        read -p "SERVICE_SECRET (e.g. some-very-secret-string): " serviceSecretPrompt
+        read -p "AMAZON_EMAIL (e.g. amazon_account@email.address): " amazonEmailPrompt
+        read -p "AMAZON_PASSWORD (e.g. Very_Secret_Amazon_Account_Password): " amazonPasswordPrompt
+        read -p "AMAZON_MFA_SECRET (e.g. 1234 5678 9ABC DEFG HIJK LMNO PQRS TUVW XYZ0 1234 5678 9ABC DEFG): " amazonMfaPrompt
+        read -p "WIFI_SSID (e.g. My FRITZ!Box 1234): " wifiSsid
+        read -p "WIFI_PASS (e.g. topsecret): " wifiPass
+        read -p "GITHUB_ACCESS_TOKEN (e.g. yoursupersecretaccesstoken): " githubAccessToken
+        touch .env
+        echo "SERVICE_USER=${serviceUserPrompt}" >> .env
+        echo "SERVICE_PASSWORD=${servicePasswordPrompt}" >> .env
+        echo "SERVICE_SECRET=${serviceSecretPrompt}" >> .env
+        echo "AMAZON_EMAIL=${amazonEmailPrompt}" >> .env
+        echo "AMAZON_PASSWORD=${amazonPasswordPrompt}" >> .env
+        echo "AMAZON_MFA_SECRET=${amazonMfaPrompt}" >> .env
+        echo "WIFI_SSID=${wifiSsid}" >> .env
+        echo "WIFI_PASS=${wifiPass}" >> .env
+        echo "GITHUB_ACCESS_TOKEN=${githubAccessToken}" >> .env
+      else
+        echo ".env file already exists. Skipping."
+    fi
 
-    echo "Aborting, because manual action for installing SSL certs is necessary!"
+    echo "Checking SSL setup..."
+    if [[ "$(ls -A ssh | grep -i \\.pem\$)" ]]
+      then
+        echo "pem files found in ssh/ dir, so SSL seems to be set up."
+      else
+        echo "Aborting. Manual action for installing SSL certs is necessary! See ./letsencrypt-authenticator/READEME.md"
+        exit 1
+    fi
 
-    # cd ../docker-infrastructure
-    # echo "Building containers for PRODUCTION..."
-    # sudo docker-compose build --build-arg PRODUCTION=true
-    # echo "Starting containers..."
-    # sudo docker-compose up -d
+    echo "Building containers for PRODUCTION..."
+    sudo docker-compose build --build-arg PRODUCTION=true
+    echo "Starting containers..."
+    sudo docker-compose up -d
 
 fi
