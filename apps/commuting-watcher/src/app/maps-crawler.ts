22cec2709963103d8dd6fa37ca5ea00e78e33fe9
Philippe Schild <Philippe.Schild@adesso.de>
2021-09-06 16:02:46 +0200

    fixed maps crawler

--- apps/commuting-watcher/src/app/maps-crawler.ts
+++ apps/commuting-watcher/src/app/maps-crawler.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as isPi from 'detect-rpi';
 import { isDocker, log } from '@smart-home-conx/utils';
 import { format } from 'date-fns';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PreferenceService } from '@smart-home-conx/preference';
 
 @Injectable()
@@ -26,70 +26,69 @@ export class GoogleMapsCrawler {
       };
     }
 
-    const screenshotsFolderPath = path.join(__dirname, 'assets', 'screenshots');
-    const browser = await puppeteer.launch(launchOptions);
-    const page = await browser.newPage();
+    let browser;
+    try {
+      const screenshotsFolderPath = path.join(__dirname, 'assets', 'screenshots');
+      browser = await puppeteer.launch(launchOptions);
+      const page = await browser.newPage();
 
-    // pass logs within headless browser to main console
-    page.on('console', consoleObj => {
-      if (consoleObj.type() === 'log') {
-        log(consoleObj.text());
-      }
-    });
-
-    log('Go to page ...');
-    // Example: https://www.google.de/maps/dir/51.5045685,6.9971393/51.668189,6.148282/data=!3m1!4b1!4m2!4m1!3e0
-    await page.goto(
-      `https://www.google.de/maps/dir/${origin.latitude},${origin.longitude}/${destination.latitude},${destination.longitude}/data=!3m1!4b1!4m2!4m1!3e0`
-    );
-    // log('Wait for travel modes visible ...');
-    // await page.waitFor('.directions-travel-mode-selector');
-    // log('Click icon for choosing "drive" as travel mode ...');
-    // await page.click('.travel-mode:nth-child(2) button');
-    // log('Wait 2s ...');
-    // await page.waitFor(2000);
-    log('Check if we need to accept cookies ...');
-    const acceptButton = await page.$x('.//button/span[contains(text(), "Ich stimme zu")]');
-    if (!!acceptButton && acceptButton.length) {
-      log('Accept cookies ...');
-      acceptButton[0].click();
-    }
-
-    log('Wait for trips visible ...');
-    await page.waitFor('.section-directions-trip');
-
-    if (await this.preferenceService.getValueFor('saveScreenshotsEnabled')) {
-      log('Saving screenshot ...');
-      await page.screenshot({
-        path: path.join(screenshotsFolderPath, `maps-${format(new Date(), 'yyyy-MM-dd_HH-mm-ss-SSS')}.png`),
-        // clip: { x: 435, y: 50, width: 1024 - 435, height: 768 - 50 * 2 }
-        // clip: { x: 435, y: 0, width: 1024 - 435, height: 768 }
-      });
-    }
-
-    log('Evaluating page ...');
-    const durationsForCar = await page.evaluate(() => {
-      const drivePossibilities = document.querySelectorAll('.section-directions-trip-travel-mode-icon');
-      console.log(`Found ${drivePossibilities.length} travel-mode-icons`);
-      const allDurations = [];
-      [].forEach.call(drivePossibilities, el => {
-        const durationContainer = el.parentNode.querySelector('.section-directions-trip-duration > span:first-child');
-        if (durationContainer) {
-          console.log(`Found durationContainer`);
-          const duration = durationContainer.textContent;
-          allDurations.push(duration);
-        } else {
-          console.log(`Did NOT found durationContainer!`);
+      // pass logs within headless browser to main console
+      page.on('console', consoleObj => {
+        if (consoleObj.type() === 'log') {
+          log(consoleObj.text());
         }
       });
-      console.log(`Found ${allDurations.length} durations`);
-      return allDurations;
-    });
 
-    log('Closing browser ...');
-    await browser.close();
+      log('Go to page ...');
+      // Example: https://www.google.de/maps/dir/51.5045685,6.9971393/51.668189,6.148282/data=!3m1!4b1!4m2!4m1!3e0
+      await page.goto(
+        `https://www.google.de/maps/dir/${origin.latitude},${origin.longitude}/${destination.latitude},${destination.longitude}/data=!3m1!4b1!4m2!4m1!3e0`
+      );
+      log('Check if we need to accept cookies ...');
+      const acceptButton = await page.$x('.//button/span[contains(text(), "Ich stimme zu")]');
+      if (!!acceptButton && acceptButton.length) {
+        log('Accept cookies ...');
+        acceptButton[0].click();
+      }
 
-    return durationsForCar.map(time => this.parseDuration(time));
+      log('Wait for trips visible ...');
+      // take the first trip as indicator for the page finished loading
+      await page.waitFor(`#section-directions-trip-0`);
+
+      if (await this.preferenceService.getValueFor('saveScreenshotsEnabled')) {
+        log('Saving screenshot ...');
+        await page.screenshot({
+          path: path.join(screenshotsFolderPath, `maps-${format(new Date(), 'yyyy-MM-dd_HH-mm-ss-SSS')}.png`),
+          // clip: { x: 435, y: 50, width: 1024 - 435, height: 768 - 50 * 2 }
+          // clip: { x: 435, y: 0, width: 1024 - 435, height: 768 }
+        });
+      }
+
+      log('Evaluating page ...');
+      const durationsForCar = await page.evaluate(() => {
+        const DURATION_ROW_SELECTOR = `section-directions-trip-`;
+        const DURATION_SELECTOR = `trip-duration`;
+
+        // loop over all trips and collect raw duration string for each
+        return Array.from(document.querySelectorAll(`[id^="${DURATION_ROW_SELECTOR}"]`))
+          .filter(e => e.id.match(new RegExp(`${DURATION_ROW_SELECTOR}\\d$`)))
+          .map(row => Array.from(row.querySelectorAll('div'))
+            .filter(e => e.className.match(DURATION_SELECTOR))
+            .map(e => e.textContent)
+          )
+          .reduce((acc, val) => acc.concat(val), []);
+      });
+      log(`raw durations: ${durationsForCar.join(',')}`);
+      log(`parsed durations: ${durationsForCar.map(time => this.parseDuration(time))}`);
+      return durationsForCar.map(time => this.parseDuration(time));
+
+    } catch(error) {
+      throw new HttpException(`Puppeteer parsing failed: ${error}`, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    } finally {
+      log('Closing browser ...');
+      await browser.close();
+    }
   }
 
   private parseDuration(rawDuration: string): number {
